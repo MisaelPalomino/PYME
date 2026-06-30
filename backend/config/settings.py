9064a9650820570dda8c2725af6395a7ec1517ae
JuanCarlosCastroBarreda <jcastrobar@unsa.edu.pe>
2026-06-29 20:45:20 -0500

    Frontend trabajado con implementacion en Categorias y Productos

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "apps.authentication",
     "apps.core",
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "authentication.Usuario"
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -139,3 +141,5 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.BrowsableAPIRenderer",
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
