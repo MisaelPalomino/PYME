efd1045c8f4b5b8c3cc3c206ef1b095ac127d764
Alberto Torres <acc.dummyagta1@gmail.com>
2026-07-06 10:27:14 -0500

    Arreglado errores de tablas inexistentes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("api/auth/", include("apps.authentication.urls")),
     path("api/core/", include("apps.core.urls")),
     path("api/inventario/", include("apps.inventario.urls")),
+    path("api/informes/", include("apps.informes.urls")),
     path("api/pedidos/", include("apps.pedidos.urls")),
     path("api/ia/", include("apps.ia.urls")),
     path("api/proveedores/", include("apps.proveedores.urls")),
