8ef4fdd259f1f6b96dae533bfc04890c6dc356d0
doomed-phobos <the_ekj.2121@outlook.com>
2026-07-05 22:29:59 -0500

    feat(Tabla): se añadió orden por defecto

--- frontend/app/components/Table.tsx
+++ frontend/app/components/Table.tsx
@@ -25,6 +25,7 @@ export type TableProps<T extends RowData> = {
   data: T[],
   columns: ColumnDef<T, any>[],
   filters: Filter[],
+  defaultSort?: SortingState,
 };
 
 function FilterInput(props: {
@@ -101,12 +102,12 @@ export function createSortableHeader<TData, TValue>(name: string) {
   };
 }
 
-export function TableWireframe<T extends RowData>({ data, columns, filters, children }: TableProps<T> & { children?: (table: Table<T>) => React.ReactNode }) {
-  const [sorting, setSorting] = useState<SortingState>([]);
+export function TableWireframe<T extends RowData>({ data, columns, defaultSort, filters, children }: TableProps<T> & { children?: (table: Table<T>) => React.ReactNode }) {
+  const [sorting, setSorting] = useState<SortingState>(defaultSort ?? []);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
     filters
       .filter(f => f.defaultValue)
-      .map(f => ({id: f.columnName, value: f.defaultValue}))
+      .map(f => ({ id: f.columnName, value: f.defaultValue }))
   );
   const table = useReactTable({
     data,
@@ -156,9 +157,9 @@ export function TableWireframe<T extends RowData>({ data, columns, filters, chil
   )
 }
 
-export function TableList<T extends RowData>({ data, columns, filters }: TableProps<T>) {
+export function TableList<T extends RowData>({ data, columns, defaultSort, filters }: TableProps<T>) {
   return (
-    <TableWireframe columns={columns} data={data} filters={filters}>
+    <TableWireframe columns={columns} data={data} filters={filters} defaultSort={defaultSort}>
       {
         (table) => (
           <Card className="p-0">
@@ -217,9 +218,9 @@ export function TableList<T extends RowData>({ data, columns, filters }: TablePr
   )
 }
 
-export function TableCard<T extends RowData>({ data, columns, filters, children }: TableProps<T> & { children: (item: T) => React.ReactNode }) {
+export function TableCard<T extends RowData>({ data, columns, filters, defaultSort, children }: TableProps<T> & { children: (item: T) => React.ReactNode }) {
   return (
-    <TableWireframe columns={columns} data={data} filters={filters}>
+    <TableWireframe columns={columns} data={data} filters={filters} defaultSort={defaultSort}>
       {
         (table) => (
           <>
